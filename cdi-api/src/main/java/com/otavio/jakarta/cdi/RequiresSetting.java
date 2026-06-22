79b40c638bcbbcea3ada3d4db18883605f1bc48e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-22 19:37:17 +0100

    feat: add RequiresSetting annotation for type, method, and field targets

---
+++ cdi-api/src/main/java/com/otavio/jakarta/cdi/RequiresSetting.java
@@ -0,0 +1,16 @@
+package com.otavio.jakarta.cdi;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD})
+public @interface RequiresSetting {
+}
