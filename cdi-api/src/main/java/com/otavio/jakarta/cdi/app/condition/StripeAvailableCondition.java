9e54d3e8f3b6a9bc667d0af9cfb94c5a2454387c
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-22 20:12:41 +0100

    refactor: remove @ApplicationScoped from StripeAvailableCondition

--- cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/StripeAvailableCondition.java
+++ cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/StripeAvailableCondition.java
@@ -2,9 +2,7 @@ package com.otavio.jakarta.cdi.app.condition;
 
 
 import com.otavio.jakarta.cdi.Condition;
-import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
 public class StripeAvailableCondition implements Condition {
 
     @Override
