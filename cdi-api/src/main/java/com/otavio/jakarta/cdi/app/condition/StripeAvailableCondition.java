e12c0238cd7726605d9b8554537ff2d3ff50ae49
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-22 20:10:43 +0100

    refactor: use PaymentGateway.STRIPE_AVAILABLE in StripeAvailableCondition

--- cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/StripeAvailableCondition.java
+++ cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/StripeAvailableCondition.java
@@ -2,7 +2,9 @@ package com.otavio.jakarta.cdi.app.condition;
 
 
 import com.otavio.jakarta.cdi.Condition;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class StripeAvailableCondition implements Condition {
 
     @Override
