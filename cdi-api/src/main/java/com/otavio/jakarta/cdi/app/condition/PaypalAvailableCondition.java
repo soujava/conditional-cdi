aeac6ca21566752340c456148912aea703760719
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-22 20:12:13 +0100

    feat: add CDI SE app entrypoint and scope StripeAvailableCondition as bean

--- cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/PaypalAvailableCondition.java
+++ cdi-api/src/main/java/com/otavio/jakarta/cdi/app/condition/PaypalAvailableCondition.java
@@ -2,9 +2,7 @@ package com.otavio.jakarta.cdi.app.condition;
 
 
 import com.otavio.jakarta.cdi.Condition;
-import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
 public class PaypalAvailableCondition implements Condition {
 
     @Override
