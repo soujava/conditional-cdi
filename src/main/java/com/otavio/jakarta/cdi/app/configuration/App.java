d8a1f6adae04e9af518c6b072732a0abc0702719
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-24 15:21:46 +0100

    chore: set app.country to BR in App bootstrap

--- src/main/java/com/otavio/jakarta/cdi/app/configuration/App.java
+++ src/main/java/com/otavio/jakarta/cdi/app/configuration/App.java
@@ -3,7 +3,7 @@ package com.otavio.jakarta.cdi.app.configuration;
 public class App {
 
     public static void main(String[] args) {
-        System.setProperty("app.country", "PT");
+        System.setProperty("app.country", "BR");
         try (var container = jakarta.enterprise.inject.se.SeContainerInitializer.newInstance().initialize()) {
             TaxCalculator taxCalculator = container.select(TaxCalculator.class).get();
             System.out.println("Tax for 100: " + taxCalculator.calculate(new java.math.BigDecimal("100")));
